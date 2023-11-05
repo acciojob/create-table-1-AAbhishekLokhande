2794cd47513b69bea38c880772036e2dd2cdb06d
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-11-05 22:47:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let table = document.querySelectorAll("#sampleTable")
+let btn = document.querySelectorAll("#btn");
 function insert_Row() {
     //Write your code here
-  
-  
+ let newRow = document.createElement("tr");
+	let cell1 = document.createElement("td");
+	cell1.innerText(NewCell1);
+	let cell2 = document.createElement("td");
+	cell2.innerText(NewCell2);
+  table.insertBefore(tr,table);
+	newRow.append(cell1,cell2);
 }
+function OnClickInsertBtn(){
+	
+}
+
+btn.addEventListner("click", onClickInsertBtn);
